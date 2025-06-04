b0cb6225c385e56e71b18b38f2ad8043cd7b4da6
mateusXLR8 <mateuspedroso.senai@gmail.com>
2025-06-03 21:50:18 -0300

    tarefa

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
+const entradaTarefa = document.getElementById("entradaTarefa");
+const listaTarefas = document.getElementById("listaTarefas");
+
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+carregarTarefas();
+
 function adicionarTarefa() {
-    console.log("Teste aula funcionou!");
+  let texto = entradaTarefa.value.trim();
+  if (texto != "") {
+    tarefas.push(entradaTarefa.value);
+    entradaTarefa.value = "";
+    salvarTarefas();
+    carregarTarefas();
+  } else {
+    alert("Tarefa inválida");
+  }
 }
-x
+
+entradaTarefa.addEventListener("keypress", function (tecla) {
+  if (tecla.key === "Enter") {
+    adicionarTarefa();
+  }
+});
+
+function carregarTarefas() {
+  listaTarefas.innerHTML = "";
+  tarefas.forEach((tarefa, posicao) => {
+    const item = document.createElement("li");
+    item.className = "item-lista";
+    item.innerHTML = `
+    <span class="item">${tarefa}</span>
+    <button id="botaoRemover" onclick="removerTarefa(${posicao})">X</button>
+    `;
+    listaTarefas.appendChild(item);
+  });
+}
+
+function removerTarefa(posicao) {
+  tarefas.splice(posicao, 1);
+  salvarTarefas();
+  carregarTarefas();
+}
+
+function salvarTarefas() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
